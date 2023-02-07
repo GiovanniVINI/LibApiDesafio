cf3d768e5f07fac47989437ec6be7237c043325a
vinicius_giovanni <viniciusmahssan@gmail.com>
2023-02-07 17:17:20 -0300

    Adicionando mudanças :rocket:

--- config/routes.js
+++ config/routes.js
@@ -1,15 +1,50 @@
 const express = require('express');
 const routes = express.Router();
 
-let lib = [
-  { "1" : { name: "The Lightning Thief", author: "Rick Riordan"} },
-  { "2" : { name: "Clean Code", author: "Robert Cecil Martin"} },
-  { "3" : { name: "Harry Potter and the Philosopher's Stone", author: "J. K. Rowling"} }
+let books = [
+  { id: 1, name: "The Lightning Thief", author_id: null },
+  { id: 2, name: "Clean Code"},
+  { id: 3, name: "Harry Potter and the Philosopher's Stone"}
 ]
 
 // Search data
-routes.get('/', (req, res) => {
-    return res.json(lib)
+routes.get('/books', (req, res) => {
+  const data = { 
+    total: books.length,
+    items: books
+  }
+  return res.json(data)
+})
+
+// Insert data
+routes.post('/books', (req, res) => {
+  const body = req.body
+
+  if (!body)
+    return res.status(201).end()
+
+    books.push(body)
+      return res.json(body)
+})
+
+// Update data
+routes.put('/books/:id', (req, res) => {
+  const bookId = parseInt(req.params.id)
+  const book = books.find(book => book.id === bookId)
+  console.log(bookId)
+
+  if (!book)
+    return res.status(404).end()
+
+  const updateBooks = books.map((book) => {
+    if (book.id == bookId) {
+      book = req.body.books
+    }
+    return book
+  })
+  books = updateBooks
+
+  return res.status(200).json(books)
 })
 
 module.exports = routes
